5d621e8710d4573883a33b4cecce9b860ad843bc
vagrant <vagrant@homestead>
2020-05-11 08:16:30 +0000

    管理员删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'wddgdys';
         $user->email = 'wddgdys@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
